f5c8d51a0cb907fd447aeaabd2d174adf5def4fb
Owen Jow <owenjow@berkeley.edu>
2017-10-07 18:07:28 -0700

    Increase throttle, add debugging output

--- carrettu/vehicles.py
+++ carrettu/vehicles.py
@@ -9,13 +9,12 @@ import time
 
 class BaseVehicle:
     def __init__(self,
-                 drive_loop_delay = .5,
+                 drive_loop_delay=0.5,
                  camera=None,
                  actuator_mixer=None):
+        self.drive_loop_delay = drive_loop_delay # how long to wait between loops
 
-        self.drive_loop_delay = drive_loop_delay #how long to wait between loops
-
-        #these need tobe updated when vehicle is defined
+        # these need tobe updated when vehicle is defined
         self.camera = camera
         self.actuator_mixer = actuator_mixer
 
@@ -49,7 +48,7 @@ class BaseVehicle:
 class TestVehicle(BaseVehicle):
 
     def __init__(self,
-                 drive_loop_delay = .5,
+                 drive_loop_delay=0.5,
                  camera=None,
                  actuator_mixer=None):
         BaseVehicle.__init__(self, drive_loop_delay, camera, actuator_mixer)
@@ -60,28 +59,30 @@ class TestVehicle(BaseVehicle):
         # Wait 30 sec before starting the script
         # time.sleep(30)  # or not (-o)
 
-        #drive loop
+        # drive loop
         while True:
+            print('next loop iteration')
             now = time.time()
 
             # Stop the loop if it has run for 60 sec
             if (now - start_time) >= 40:
+                print('[o] Done.')
                 break
 
             start = now
 
-            #get image array image from camera (threaded)
-            img_arr = self.camera.capture_arr()
+            # get image array image from camera (threaded)
+            # img_arr = self.camera.capture_arr()
 
             # Set angle, throttle, and drive_mode depending on img_arr
             angle = 0
-            throttle = 0.1
+            throttle = 0.2
 
             self.actuator_mixer.update(throttle, angle)
 
             # print current car state
             end = time.time()
             lag = end - start
-            print('\r CAR: angle: {:+04.2f}   throttle: {:+04.2f}  lag: {:+04.2f}'.format(angle, throttle, lag), end='')
+            print('\r CAR: angle: {:+04.2f}   throttle: {:+04.2f}  lag: {:+04.2f}'.format(angle, throttle, lag))
             
             time.sleep(self.drive_loop_delay)
