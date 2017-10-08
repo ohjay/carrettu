91b7b4d757e6d89f04118178a54ef109d354a09e
Owen Jow <owenjow@berkeley.edu>
2017-10-07 19:13:16 -0700

    Update __init__ file and documentation

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Original: [http://www.donkeycar.com](http://www.donkeycar.com/)
 ### Drive your car
 Once you have built your car, you can use it like this.
 
-1. Start your car and connect it to the pilot server. `python scripts/drive.py`.
+1. Start your car. `python scripts/drive.py`.
--- carrettu/__init__.py
+++ carrettu/__init__.py
@@ -2,25 +2,16 @@ import os
 uname = os.uname()
 if not uname[4].startswith('arm'):
     print('Loading modules for server.')
-    from . import (utils, 
-                   models, 
-                   datasets, 
-                   sessions,
-                   remotes, 
+    from . import (utils,
                    sensors,
                    actuators,
                    mixers,
                    vehicles,
-                   pilots,
-                   templates,
                    config) 
 else:
     print('Detected running on Raspberry Pi. Only importing select modules.')
     from . import (actuators, 
                    mixers, 
-                   remotes, 
                    sensors, 
                    vehicles, 
-                   config,
-                   pilots,
-                   models)
+                   config)
